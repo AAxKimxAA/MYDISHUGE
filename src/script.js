516d862a03aed4dabd057fae1d9d45f0765dcf37
AAxKimxAA <130959645+AAxKimxAA@users.noreply.github.com>
2023-06-26 13:40:05 +0800

    Delete src directory

--- src/script.js
+++
@@ -1,9 +0,0 @@
-document.write("")
-
-var count = 0;
-    var counterElement = document.getElementById("counter");
-
-    function increment() {
-      count+=2;
-      counterElement.textContent = count;
-    }
